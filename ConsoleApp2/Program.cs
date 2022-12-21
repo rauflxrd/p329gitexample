bd57d68ef851c2ca6c156fae19e3599de2b7d1c9
[rauf] <[raufam@code.edu.az]>
2022-12-21 19:59:15 +0400

    init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A-ni daxil et:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("B-ni daxil et:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = a + b;
+
+            Console.WriteLine($"{a}+{b}={c}");
+        }
+    }
+}
